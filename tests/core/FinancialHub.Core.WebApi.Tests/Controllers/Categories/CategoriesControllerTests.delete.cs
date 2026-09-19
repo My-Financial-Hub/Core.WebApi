f2310da991c1bec179b7c331d06ccec34d0ffc3e
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-09-19 17:33:03 -0300

    Fixes Categories Endpoint response

--- tests/core/FinancialHub.Core.WebApi.Tests/Controllers/Categories/CategoriesControllerTests.delete.cs
+++ tests/core/FinancialHub.Core.WebApi.Tests/Controllers/Categories/CategoriesControllerTests.delete.cs
@@ -4,9 +4,9 @@
     {
         [Test]
         [TestCase(Description = "Delete Returns NoContent", Category = "Delete")]
-        public async Task DeleteMyCategories_ServiceSuccess_ReturnsNoContent()
+        public async Task Delete_ServiceSuccess_ReturnsNoContent()
         {
-            var response = await this.controller.DeleteCategory(Guid.NewGuid());
+            var response = await this.controller.Delete(Guid.NewGuid());
 
             var result = response as ObjectResult;
 
--- tests/core/FinancialHub.Core.WebApi.Tests/Controllers/Categories/CategoriesControllerTests.get.cs
+++ tests/core/FinancialHub.Core.WebApi.Tests/Controllers/Categories/CategoriesControllerTests.get.cs
@@ -17,7 +17,7 @@ namespace FinancialHub.Core.WebApi.Tests.Controllers
                 .ReturnsAsync(mockResult)
                 .Verifiable();
 
-            var response = await this.controller.GetCategories();
+            var response = await this.controller.GetAll();
             var result = (ObjectResult)response;
 
             Assert.AreEqual(200, result.StatusCode);
